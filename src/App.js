5cd37b5ecabbd88d5f0e794dc19b5f50b2669c1f
Sarah Bühler <97050915+SarahBuehler@users.noreply.github.com>
2022-08-01 15:37:05 +0200

    cleaned files, add logo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import pokeball from "./media/pokeball.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pokedex</h1>
+      <img src={pokeball} alt="pokeball" className="pokeball-icon" />
     </div>
   );
 }
